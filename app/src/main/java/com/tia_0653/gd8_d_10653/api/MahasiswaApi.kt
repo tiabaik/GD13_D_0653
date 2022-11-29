7a35774978ec18641b844e9b2523b024219aa0fd
tiabaik <106868597+tiabaik@users.noreply.github.com>
2022-11-29 09:06:29 +0700

    GD13_D_0653

---
+++ app/src/main/java/com/tia_0653/gd8_d_10653/api/MahasiswaApi.kt
@@ -0,0 +1,13 @@
+package com.tia_0653.gd8_d_10653.api
+
+class MahasiswaApi {
+    companion object{
+        val  BASE_URl = "https://6368bba315219b84960558d7.mockapi.io/api/"
+
+        val GET_ALL_URL = BASE_URl+ "mahasiswa/"
+        val GET_BY_ID_URL = BASE_URl+"mahasiswa/"
+        val ADD_URL = BASE_URl+"mahasiswa"
+        val UPDATE_URL = BASE_URl+"mahasiswa/"
+        val DELETE_URL = BASE_URl+"mahasiswa/"
+    }
+}
---
+++ app/src/main/java/com/tia_0653/gd8_d_10653/models/Mahasiswa.kt
@@ -0,0 +1,5 @@
+package com.tia_0653.gd8_d_10653.models
+
+class Mahasiswa(var nama: String, var npm: String, var fakultas: String, var prodi: String) {
+    var id: Long? = null
+}
